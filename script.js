3f47363398b7cd7ec75f532cab7d37f439e81bcb
bananinhaa <rafa.oliveira2003@gmail.com>
2020-07-27 12:37:19 -0300

    commit

--- script.js
+++ script.js
@@ -3,6 +3,14 @@ function imc(){
  peso = document.getElementById('peso').value
  altura = document.getElementById('altura').value
  resultado =  parseFloat(peso) / (parseFloat(altura)*parseFloat(altura))  
- document.getElementById('resultado').value = resultado
+ document.getElementById('resultado').value = resultado 
+
+ if (peso == "") {
+    document.getElementById('erro').innerHTML="<div class='erro' id='erro'>Preencha de forma adequada</div>"
+ }
+ if (altura == "") {
+    document.getElementById('erro').innerHTML="<div class='erro' id='erro'>Preencha de forma adequada</div>"
+ }
 }
+
     
